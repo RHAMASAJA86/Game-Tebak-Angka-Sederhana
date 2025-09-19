7cbd28089c68215cd0c029b950997055f9c234a2
ubaydillah <m.ubaiydillah.r@gmail.com>
2025-09-19 08:24:45 +0700

    commit2

--- src/gametebakangka/GameTebakAngka.java
+++ src/gametebakangka/GameTebakAngka.java
@@ -1,7 +1,20 @@
 package gametebakangka;
+import javax.swing.JOptionPane;
+import java.util.Random;
 
 public class GameTebakAngka {
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Buat angka random 1-100
+        Random pp = new Random();
+        int angkaBenar = pp.nextInt(100) + 1;
+        int tebakan;
+        int percobaan = 0;
+        boolean bebnar = false;
+        
+        JOptionPane.showMessageDialog(null,
+            "Selamat datang di Game Tebak Angka!\n" +
+            "Saya sudah memilih angka 1-100.\n" +
+            "Coba tebak ya...");
+           
     }    
 }
